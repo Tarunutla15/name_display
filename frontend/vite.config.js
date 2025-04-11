497f7691a95906250687c6819bffbd2ac9be86c2
tarunutla <tarun.ut@tmachinesoftware.com>
2025-04-11 10:49:54 +0530

    package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/', // ✅ important for Netlify to find assets
-  build: {
-    outDir: 'dist', // ✅ make sure it goes to root/dist
-  },
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),tailwindcss()],
 })
